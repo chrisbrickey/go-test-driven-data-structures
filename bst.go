0fb2eaca384d0a25940e63f23fdd9cffa05b9254
chrisbrickey <cbrickey1@gmail.com>
2018-01-22 08:15:56 -0800

    draft code for adding second node

--- bst.go
+++ bst.go
@@ -8,7 +8,24 @@ func(b *BST) add(n int32) {
 	var newNode = Node{n, nil, nil, nil}
 	if b.root == nil {
 		b.root = &newNode
+		return
 	}
+
+	current := b.root //Node
+
+	if n < current.value {
+		if current.left != nil {
+
+		}
+	}
+
+	for current != nil {
+		if n > current {
+
+		}
+	}
+
+
 }
 
 
