0c5f18d3e7290d5053ee26c87164e6b1d9f8ef0f
chrisbrickey <cbrickey1@gmail.com>
2018-01-21 22:14:42 -0800

    write test for and implement add to fill root first

--- bst.go
+++ bst.go
@@ -3,3 +3,11 @@ package goBST
 type BST struct {
 	root *Node
 }
+
+func(b BST) add(n int32) {
+	var newNode = Node{n, nil, nil, nil}
+	if b.root == nil {
+		b.root = &newNode
+	}
+}
+
--- bst_test.go
+++ bst_test.go
@@ -8,12 +8,42 @@ import (
 
 func TestEmptyTreeHasNilRoot(t *testing.T) {
 	var tree = BST{}
-	emptyRoot := tree.root
+	actual := tree.root
 
-	if emptyRoot != nil {
+
+	if actual != nil {
 		t.Errorf("TestEmptyTreeHasNilRoot: tree.root failed")
 		fmt.Println("Expected: ", nil)
-		fmt.Println("Actual: ", emptyRoot)
+		fmt.Println("Actual: ", actual)
 	}
 
 }
+
+
+func TestRootNotNilAfterAdd(t *testing.T) {
+	var tree = BST{}
+	tree.add(11)
+	actual := tree.root
+
+	if actual != nil {
+		t.Errorf("TestRootNotNilAfterAdd: tree.root failed")
+		fmt.Println("Expected: ", nil)
+		fmt.Println("Actual: ", actual)
+	}
+
+}
+
+//func TestFirstValueAssignedToRoot(t *testing.T) {
+//	var tree = BST{}
+//	tree.add(11)
+//	actual := tree.root.value
+//	var expectation int32 = 11
+//
+//	if actual != expectation {
+//		t.Errorf("TestFirstValueAssignedToRoot: tree.root.value failed")
+//		fmt.Println("Expected: ", expectation)
+//		fmt.Println("Actual: ", actual)
+//	}
+//
+//}
+
